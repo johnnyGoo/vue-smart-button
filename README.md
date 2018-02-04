10f9b30d2ef371e7fc8819f38b6e1e1f97f3fb56
Johnny <157781745@qq.com>
2018-02-04 21:43:16 +0800

    v1.0.2

---
+++ README.md
@@ -0,0 +1,28 @@
+# vue-smart-button
+##### A Vue.js Component
+### Usage
+```
+npm install vue-smart-button --save
+import VueSmartButton from 'vue-smart-button'
+```
+```
+npm run dev
+npm run build
+```
+
+
+### Props:
+```
+
+
+```
+### Methods
+```
+
+
+```
+### Properties
+```
+
+```
+
---
+++ src/component.vue
@@ -0,0 +1,33 @@
+<template>
+    <div @touchstart="touchstart" @touchend="touchend" style="user-select: none">
+        <slot v-if="down" name="down"></slot>
+        <slot v-if="!down" name="up"></slot>
+
+    </div>
+</template>
+<style scoped>
+
+</style>
+
+<script>
+    // 注册
+    export default {
+        name: 'vue-smart-button',
+        // 声明 props
+        props: {},
+        data: function () {
+            return {down: false}
+        },
+        methods: {
+            touchstart: function () {
+                this.down = true
+            },
+            touchend: function () {
+                this.down = false
+            }
+        },
+        mounted: function () {
+
+        }
+    }
+</script>
